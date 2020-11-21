c2c5438b2ede888d30203763ee2f405db0ab0a48
Harshil <ghagrawal17@gmail.com>
2020-11-21 18:49:49 +0530

    ✨intial commits

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,68 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+// import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home({ jobs }) {
   return (
-    <div className={styles.container}>
+    <div className="contianer mx-auto">
       <Head>
-        <title>Create Next App</title>
+        <title>Hire Juniors</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="px-64 pt-16">
+        <h1 className="text-5xl text-center">Welcome to hirejuniors.dev</h1>
+        <div className="my-4">
+          <ul>
+            {jobs.map((job) => {
+              const {
+                URL,
+                id,
+                Company,
+                Location,
+                Position,
+                Categories,
+                Type,
+              } = job;
+              return (
+                <li
+                  key={id}
+                  className="mt-8 p-4 rounded overflow-hidden shadow-md bg-white h-auto"
+                >
+                  <h3 className="text-2xl">
+                    {Position}{' '}
+                    <a href={URL} target="_blank">
+                      <span role="image" aria-label="link">
+                        🔗
+                      </span>
+                    </a>
+                  </h3>
+                  <ul className="text-lg">
+                    <li>Company: {Company}</li>
+                    <li>
+                      Location:{' '}
+                      <span role="image" aria-label="globe">
+                        🌐{' '}
+                      </span>
+                      {Location}
+                    </li>
+                  </ul>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch('https://hire-junior-devs.herokuapp.com/jobs');
+  const jobs = await res.json();
+  console.log(jobs);
+  return {
+    props: { jobs },
+  };
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
