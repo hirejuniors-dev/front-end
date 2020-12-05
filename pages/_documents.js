b6d921f38586bbe81a578f82104f7973a800350e
Harshil <ghagrawal17@gmail.com>
2020-12-06 03:50:49 +0530

    ⚡Add plausible link

---
+++ pages/_documents.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          {/* Plausible Analytics script */}
+          <script
+            async
+            defer
+            data-domain="hirejuniors.dev"
+            src="https://plausible.harshil.dev/js/plausible.js"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
