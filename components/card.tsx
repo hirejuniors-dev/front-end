c6dd2e3701040a8501ed75f36cd4ff386bb3dd92
Pranit Brahmbhatt <pranitbrahmbhatt10@gmail.com>
2024-02-11 08:37:04 -0500

    Company Logo Size and Font-case for Roles (#27)
    * fixed the company logo sizing across different devices and unified the font-case for roles
    
    removing the credential files and updating the cards.tsx to enhance the card's UI and company logos
    
    removed yarn.lock files
    
    * deleted the .env.dev and yarn.lock file. Enhanced the logos and cards UI
    
    ---------
    
    Co-authored-by: Pranit Brahmbhatt <pranit@esamudaay.com>

--- components/card.tsx
+++ components/card.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { getCompany } from '../utils/getCompany';
 import type { Company } from '../utils/getCompany';
+import '../styles/Home.module.css';
 
 export default async function Card({
 	role,
@@ -17,21 +18,40 @@ export default async function Card({
 		Logo: logoArr,
 	} = companyDetails.fields;
 
+	const capitalizeFirstLetter = (str) => {
+		const wordsToSkip = ['AI', 'ML', 'UX', 'UI', 'AI/ML', 'LTH', 'IT', 'IT-Techniker']; // Add more words as needed
+		const words = str.split(/\s+/);
+	
+		const capitalizedWords = words.map((word) => {
+			const isSpecialChar = /[!@#$%^&*(),.?":{}|<>-]/.test(word);
+	
+			return wordsToSkip.includes(word.toUpperCase()) || isSpecialChar
+				? word
+				: word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+		});
+	
+		return capitalizedWords.join(' ');
+	};
+
+	const unifiedRole = capitalizeFirstLetter(role);
+
 	return (
 		<Link href={applicationUrl} passHref>
 			<div className="mb-8 p-4 sm:p-6 rounded-lg overflow-hidden shadow-lg bg-white h-auto">
 				<div className="sm:flex sm:justify-between">
-					<div className="flex">
-						<Image
-							src={logoArr[0].thumbnails.large.url}
-							alt={`Logo of ${companyName}`}
-							width={75}
-							height={75}
-							className="align-middle"
-						/>
+					<div className="flex content-center items-center">
+						<div className='box-content max-w-screen-sm sm:size-2/3'>
+							<Image
+								src={logoArr[0].thumbnails.large.url}
+								alt={`Logo of ${companyName}`}
+								width={80}
+								height={50}
+								className="align-middle"
+							/>
+						</div>
 						<div className="pl-4">
 							<h3 className="text-lg sm:text-2xl max-w-md font-inter">
-								{role}{' '}
+								{unifiedRole}{' '}
 							</h3>
 							<p className="text-gray-400">{companyName}</p>
 						</div>
